d112a50a9cbe587421180d46cd8f74056820b19d
Youngseo Kang <youngseo.kangg@gmail.com>
2025-01-22 22:42:09 +0900

    react-query 관련 plugin 분리

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -2,6 +2,7 @@ const auto = require("./src/auto");
 const base = require("./src/base");
 const react = require("./src/react");
 const typescript = require("./src/typescript");
+const query = require("./src/query");
 
 module.exports = {
   extends: ["@rushstack/eslint-config/profile/web-app"],
@@ -10,5 +11,6 @@ module.exports = {
     base,
     react,
     typescript,
+    query,
   },
 };
